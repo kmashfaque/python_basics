483c76e66a9ae8dd81ba3134853e7f32016d7b90
Ashfaque Rahaman <nvjkvhdjkh@gmail.com>
2022-11-05 18:13:23 +0600

    changes

--- basics/problems.py
+++ basics/problems.py
@@ -135,32 +135,3 @@
 #     user_input = new_user_input+val
 #     new_user_input = user_input
 # print(temp)
-
-
-from collections import Counter
-import collections
-lst = [["chi", 20.0], ["beta", 50.0], ["alpha", 50]]
-
-newlist = []
-for val in lst:
-    newlist.append(val[1])
-    newlist.sort()
-    print(newlist)
-
-string = "eeaaabbbbcceedd"
-
-hash_map = {}
-
-for s in sorted(string):
-    if s not in hash_map:
-        hash_map[s] = 1
-    else:
-        hash_map[s] += 1
-
-# for w in sorted(hash_map, key=hash_map.get, reverse=True):
-
-# print(w, hash_map[w])
-z = Counter(hash_map).most_common(3)
-
-for x in z:
-    print(*x)
